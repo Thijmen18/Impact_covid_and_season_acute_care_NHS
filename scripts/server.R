5ed42234732751eccf1f45d57201f25810d7bc3b
Thijmen18 <36414676+Thijmen18@users.noreply.github.com>
2023-07-22 15:29:02 +0100

    add fixed issue with x-axis in graph

--- scripts/server.R
+++ scripts/server.R
@@ -89,7 +89,9 @@ server <- function(input, output, session) {
            fill = "Season") +
       scale_y_continuous(labels = scales::comma) +
       scale_fill_manual(values = season_colours) +
-      theme_penguin()
+      theme_penguin() +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      scale_x_continuous(breaks = c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019))
     
   })
   
